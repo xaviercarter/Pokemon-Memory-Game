09eb771df4fc520ef017fc707ddc97f2395f919f
Ecks <ecks@Eckss-MBP.attlocal.net>
2023-04-28 05:51:03 -0400

    I killed my game so I can pass the deliverable. First attempt was too complex for a week turn around time.

--- app.js
+++ app.js
@@ -1,86 +1,57 @@
-//  const variable creation
-const section = document.querySelector('section');
-const pokeballsCount = document.querySelector('span');
-let pokeballsLeft = 3;
+let flippedCard = null;
+let preventClick = false;
+let matchesFound = 0;
+let turnsLeft = 5;
 
-// add the pokeballsCount to the text field
-pokeballsCount.textContent = pokeballsLeft;
-
-//create an array of objects to access the cards and info for the cards. an image is linked to its name
-const getCards = () => [ 
-    { imgSrc: "./images/bulbasaur-front.png", pokemon: "bulbasaur", imgSrcBack: "./images/back-of-card.png", },
-    { imgSrc: "./images/squirtle-front.png", pokemon: "squirtle", imgSrcBack: "./images/back-of-card.png", },
-    { imgSrc: "./images/charmander-front.png", pokemon: "charmander", imgSrcBack: "./images/back-of-card.png", },
-    { imgSrc: "./images/bulbasaur-front.png", pokemon: "bulbasaur", imgSrcBack: "./images/back-of-card.png", },
-    { imgSrc: "./images/squirtle-front.png", pokemon: "squirtle", imgSrcBack: "./images/back-of-card.png", },
-    { imgSrc: "./images/charmander-front.png", pokemon: "charmander", imgSrcBack: "./images/back-of-card.png", },
-];
-
-// now that we have the cards all in an array of objects we need a function to shuffle
-const shuffleCards = () => {
-    const cardInfo = getCards();
-    cardInfo.sort(() => Math.random() - 0.5);
-    return cardInfo;
-};
-
-// using js to generate the html using the shuffled cards this function will generate a card slot and card image
-const cardGenerator = () => {
-    const cardInfo = shuffleCards();
-
-const cards = document.querySelectorAll(".card");
-
-// loop to generate multiple cards
-    cardInfo.forEach(item => {
-        const card = document.createElement("div");
-        const front = document.createElement("img");
-        const back = document.createElement("img");
-
-// create classes for the cards
-        card.classList = "card";
-        front.classList = "front";
-        back.classList = "back";
-
-// html and classes have been created however they do not have nodes attached to them
-        section.appendChild(card);
-        card.appendChild(front);
-        card.appendChild(back);
-
-// attach the info to the cards using dot notation
-        front.src = item.imgSrc;
-        back.src = item.imgSrcBack;
-        card.setAttribute('pokemon', item.pokemon);
-
-// flip the card
-        card.addEventListener("click", (e) => {
-
- // card.classList.toggle("toggleCard");
-        if (e.target.src == item.imgSrcBack) {
-            e.target.src = item.imgSrc;
-
-        } else {
-            e.target.src = item.imgSrcBack;
-        }
-        checkCards(e);
-        });
-    });
-};
-
-//Check for a match, after flipping 2
-const checkCards = (e) => {
-    const clickedCard = e.target;
-    clickedCard.classList.add('flipped');
-    const flippedCards = document.querySelectorAll('.flipped');
-
-    if(flippedCards.length === 2){
-        if(flippedCards[0].getAttribute("pokemon") === flippedCards[1].getAttribute("pokemon")){
-                console.log("match");
-            } else {
-                console.log('wrong');
-                pokeballsLeft--;
-                pokeballsCount.textContent = pokeballsLeft;
-            } 
+// flip the card by removing the color-hidden class this reveals the color underneath
+const flipCard = (e) => {
+    const target = e.currentTarget;
+   
+    if ( preventClick || target === flippedCard || target.className.includes('correct')) {
+        return;
     }
-};
 
-cardGenerator();
+    target.className = target.className
+        .replace('color-hidden', '')
+        .trim();
+    target.className += ' correct';
+    
+// flip first card and show until second card is clicked
+    if (!flippedCard) {
+        flippedCard = target;
+        
+    } else if (flippedCard) {
+
+// after first card is flipped check for a match if no match flip card back over
+        if (flippedCard.getAttribute('data-card') !== target.getAttribute('data-card')) {
+            preventClick = true;
+            setTimeout(() => {
+                flippedCard.className = flippedCard.className.replace('correct', '').trim() +
+                ' color-hidden';
+                target.className = target.className.replace('correct', '').trim() + 
+                ' color-hidden';
+                flippedCard = null;
+                preventClick = false;
+
+// if no match found after check turnsLeft is reduced by 1
+                turnsLeft--;
+                console.log(turnsLeft);
+                document.getElementById('turnsUp').innerHTML = "Turns Left: " + turnsLeft ;
+                if (turnsLeft === 0) {
+                    console.log('you lose');
+                    document.body.innerHTML = "Sorry you lose!";
+                }
+            }, 500);
+        } else {
+            matchesFound++;
+            flippedCard = null;
+
+// create win logic and print to the screen
+            if (matchesFound === 8) {
+                console.log('you win');
+                document.body.innerHTML = "You Win!";
+            }
+        }
+    }
+}
 
